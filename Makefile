e9152f340792797fd6f86b0aabfbb9958aada9a0
Bryce <brycemwilson@gmail.com>
2016-02-26 21:09:23 -0600

    Delete procmesh component

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 LIBS  = -L. -levo2
-INCLUDES = -I../evo2
+INCLUDES = -I../evo2 -I.
 CFLAGS = -Wall
 
 SRC = $(wildcard *.c)
 CSRC=$(wildcard components/*.c)
+SYSRC=$(wildcard systems/*.c)
+SCSRC=$(wildcard scenes/*.c)
 
-moor: $(SRC) $(CSRC)
+teasel: $(SRC) $(CSRC) $(SYSRC) $(SCSRC)
 	gcc -o $@ $^ $(CFLAGS) $(INCLUDES) $(LIBS)
 
 clean:
--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include <engine.h>
+#include "debug.h"
+#include "systems/sys_render.h"
+#include "systems/sys_fps_controller.h"
+
+void demo0();
+
+/* initSys initializes the systems required for the game. */
+static void initSys()
+{
+  InitRenderSystem();
+  InitFpsControllerSystem();
+}
 
 int main(int argc, char **argv)
 {
+  debug_puts("starting engine");
   tv_EngineInit();
+  debug_puts("registering systems");
+  initSys();
 
-  /* TODO register systems */
-
-  /* TODO add initial entities */
-
+  debug_puts("starting scene");
+  demo0();
+  debug_puts("running engine");
   tv_EngineRun();
+
+  debug_puts("done");
   return 0;
 }
