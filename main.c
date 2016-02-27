f2a71679b7d4f6a706af87e343910fc8dc546755
Bryce <brycemwilson@gmail.com>
2016-02-26 21:10:16 -0600

    Reindent files

--- main.c
+++ main.c
@@ -8,22 +8,22 @@ void demo0();
 /* initSys initializes the systems required for the game. */
 static void initSys()
 {
-  InitRenderSystem();
-  InitFpsControllerSystem();
+	InitRenderSystem();
+	InitFpsControllerSystem();
 }
 
 int main(int argc, char **argv)
 {
-  debug_puts("starting engine");
-  tv_EngineInit();
-  debug_puts("registering systems");
-  initSys();
+	debug_puts("starting engine");
+	tv_EngineInit();
+	debug_puts("registering systems");
+	initSys();
 
-  debug_puts("starting scene");
-  demo0();
-  debug_puts("running engine");
-  tv_EngineRun();
+	debug_puts("starting scene");
+	demo0();
+	debug_puts("running engine");
+	tv_EngineRun();
 
-  debug_puts("done");
-  return 0;
+	debug_puts("done");
+	return 0;
 }
